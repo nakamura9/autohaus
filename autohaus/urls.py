0bb7af775be21243f8bb156d34ee81ced4a764ee
Caleb Kandoro <kandoroc@hotmail.com>
2025-11-30 20:40:02 +0200

    url routing

--- autohaus/urls.py
+++ autohaus/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('billing/', include('billing.urls')),
-    path('', include('auto_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
++ [path('', include('auto_app.urls'))]
